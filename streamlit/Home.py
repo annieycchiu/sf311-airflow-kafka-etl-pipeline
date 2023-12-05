b3b34e34b9a50218035234e8e44bc60d4acb25c1
annieycchiu <annieycchiu@gmail.com>
2023-12-05 03:01:14 -0800

    add image

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+from helpers.utils import load_image
+
 # Main App
 def main():
     # set page configuration
@@ -11,5 +13,13 @@ def main():
     # set page title
     st.title(':foggy: SF 311 Requests')
 
+    # display architecture diagram
+    st.write("<h3 style='text-align: center;'>Architecture Diagram</h3>", unsafe_allow_html=True)
+    image_path = 'assets/images/SF_311_Architecture.png'
+    image = load_image(image_path)
+    st.image(image, caption='Streaming Data ETL Pipeline')
+
 if __name__ == '__main__':
     main()
+
+# python -m streamlit run /Home.py 
