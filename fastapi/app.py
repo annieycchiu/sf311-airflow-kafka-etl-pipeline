db275e6cb05df28cbbc2d1959d87529ff92e3e45
annieycchiu <annieycchiu@gmail.com>
2023-12-06 16:09:27 -0800

    add comment

--- fastapi/app.py
+++ fastapi/app.py
@@ -1,15 +1,27 @@
-from fastapi import FastAPI
+# Third-party imports
 import joblib
 import numpy as np
+from fastapi import FastAPI
 
+
+# initialize FastAPI instance for an API application
 app = FastAPI()
 
 # load trained model
 clf = joblib.load('./models/random_forest_311_v2.joblib')
 
-# define endpoint for prediction
+
 @app.post("/predict")
 async def predict(data: dict):
+    """
+    Endpoint for making request resolved time range predictions.
+
+    Args:
+      - data (dict): Dictionary containing user input data.
+
+    Returns:
+      - dict: Dictionary containing the prediction result.
+    """
     # convert the user input data into a numpy array and reshape it
     user_input = np.array(data['data']).reshape(1, -1)
 
